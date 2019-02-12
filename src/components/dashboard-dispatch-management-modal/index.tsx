f05cfdd00d4eb96ac2d5c396a56dc738f7464909
Ryan Gaus <rsg1egoman@gmail.com>
2019-02-12 11:36:27 -0500

    code: first idea for dashboard dispatch modal
    - Basically copied the expanded report modal
    - Animations still not working right

--- src/components/dashboard-dispatch-management-modal/index.tsx
+++ src/components/dashboard-dispatch-management-modal/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import classnames from 'classnames';
 
 export default function DashboardDispatchManagementModal({
   visible,
   dispatch,
   onCloseModal,
 }) {
-  if (visible) {
-    return <div className="dashboard-dispatch-management-modal">
-      Hello world <pre>{JSON.stringify(dispatch)}</pre>
-      <button onClick={onCloseModal}>Close</button>
-    </div>;
-  } else {
-    return null;
-  }
+  return ReactDOM.createPortal(
+    (
+      <div className={classnames('dashboard-dispatch-management-modal', {visible})}>
+        <div className="dashboard-dispatch-management-modal-inner">
+          <div className="dashboard-dispatch-management-modal-header-app-bar">
+            {dispatch ? dispatch.name : 'New Dispatch'}
+          </div>
+          <div className="dashboard-dispatch-management-modal-split-container">
+            <div className="dashboard-dispatch-management-modal-split left">
+              Stuff goes here
+            </div>
+            <div className="dashboard-dispatch-management-modal-split right">
+              More stuff goes here
+              <pre>{JSON.stringify(dispatch)}</pre>
+            </div>
+          </div>
+          <div className="dashboard-dispatch-management-modal-footer-app-bar">
+            <span onClick={onCloseModal}>Cancel</span>
+            <button>Save Dispatch</button>
+          </div>
+        </div>
+      </div>
+    ),
+    document.body,
+  );
 }
