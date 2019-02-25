0d5086ffd4c4f0c876bb6c21d1b1091222ff0c1a
Gus Cost <guscost@gmail.com>
2019-02-25 14:50:55 -0500

    code: flexbox implementation of user list

--- src/components/admin-user-management/index.tsx
+++ src/components/admin-user-management/index.tsx
@@ -1,80 +1,116 @@
 import React from 'react';
 import objectSnakeToCamel from '../../helpers/object-snake-to-camel';
+import { Icons } from '@density/ui';
 
 const MOCK_DATA = [
   {
     "id": "usr_123",
     "email": "garrett@density.io",
     "full_name": "Garrett Bastable",
-    "role": "readonly"
+    "role": "readonly",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_456",
     "email": "lukasz+rounded@density.io",
     "full_name": "Lukasz Zagaja",
-    "role": "readonly"
+    "role": "readonly",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_789",
     "email": "ben@density.io",
     "full_name": "Ben Redfield",
-    "role": "admin"
+    "role": "admin",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_234",
     "email": "merati@density.io",
     "full_name": "Matt Merati",
-    "role": "readonly"
+    "role": "readonly",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_567",
     "email": "bw@roundedco.com",
     "full_name": "Brian Weinreich",
-    "role": "admin"
+    "role": "admin",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_890",
     "email": "andrew@density.io",
     "full_name": "",
-    "role": "owner"
+    "role": "owner",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_345",
     "email": "gus@density.io",
     "full_name": "Gus Cost",
-    "role": "admin"
+    "role": "admin",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_678",
     "email": "robb.nesmith+3@density.io",
     "full_name": "Robb Nesmith",
-    "role": "readonly"
+    "role": "readonly",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   },
   {
     "id": "usr_901",
     "email": "rg@roundedco.com",
     "full_name": "Robert Grazioli",
-    "role": "admin"
+    "role": "admin",
+    "last_login": "2019-02-01T00:00:00Z",
+    "invitation_status": "pending"
   }
 ]
 
 export default function AdminUserManagement({}) {
-  return <table>
-    <thead>
-      <th>Email</th>
-      <th>Name</th>
-      <th>Role</th>
-      <th>Activity</th>
-      <th>Invitation</th>
-    </thead>
-    <tbody>
-      {MOCK_DATA.map(objectSnakeToCamel).map(u => (<tr>
-        <td>{u.email}</td>
-        <td>{u.fullName}</td>
-        <td>{u.role}</td>
-        <td>{}</td>
-        <td>{"asdf"}</td>
-      </tr>))}
-    </tbody>
-  </table>;
+  const data = MOCK_DATA.map(objectSnakeToCamel);
+  return <div className="admin-user-management-list">
+    <div>
+      <div className="admin-user-management-header">Email</div>
+      {data.map(u => <div className="admin-user-management-cell">{u.email}</div>)}
+    </div>
+    <div>
+      <div className="admin-user-management-header">Name</div>
+      {data.map(u => <div className="admin-user-management-cell">{u.fullName}</div>)}
+    </div>
+    <div>
+      <div className="admin-user-management-header">Role</div>
+      {data.map(u => <div className="admin-user-management-cell">{u.role}</div>)}
+    </div>
+    <div style={{flexGrow: 1}}>
+      <div className="admin-user-management-header"></div>
+      {data.map(u => <div className="admin-user-management-cell"></div>)}
+    </div>
+    <div>
+      <div className="admin-user-management-header">Activity</div>
+      {data.map(u => <div className="admin-user-management-cell">Active</div>)}
+    </div>
+    <div>
+      <div className="admin-user-management-header">Invitation</div>
+      {data.map(u => <div className="admin-user-management-cell">{u.invitationStatus}</div>)}
+    </div>
+    <div>
+      <div className="admin-user-management-header"></div>
+      {data.map(u => <div className="admin-user-management-cell">{u.invitationStatus === 'pending' ? 'Resend' : ''}</div>)}
+    </div>
+    <div>
+      <div className="admin-user-management-header"></div>
+      {data.map(u => <div className="admin-user-management-cell"><Icons.Trash /></div>)}
+    </div>
+  </div>;
 }
