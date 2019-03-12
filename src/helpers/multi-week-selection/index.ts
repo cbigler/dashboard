8b0c8193cc40067ee58435b074f9962cc94f8c37
Brian Weinreich <be.weinreich@gmail.com>
2019-03-12 13:27:20 -0400

    Update index.ts

--- src/helpers/multi-week-selection/index.ts
+++ src/helpers/multi-week-selection/index.ts
@@ -4,8 +4,5 @@ export default function isMultiWeekSelection(startDate, endDate) {
 	let multiWeekSelection = false;
 	const startDateMoment = moment(startDate);
 	const endDateMoment = moment(endDate);
-	if (endDateMoment.diff(startDateMoment, 'days') > 7) {
-		multiWeekSelection = true;
-	}
-	return multiWeekSelection;
+	return endDateMoment.diff(startDateMoment, 'days') > 7;
 }
