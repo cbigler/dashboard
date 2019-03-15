009773f94ad66b4341b436491ff5f297f686e2ce
Gus Cost <guscost@gmail.com>
2019-03-15 15:45:16 -0400

    code: use working modal helpers from live branch

--- src/actions/modal/show.ts
+++ src/actions/modal/show.ts
@@ -1,15 +1,8 @@
-import hideModal from './hide';
-
 export const TRANSITION_TO_SHOW_MODAL = 'TRANSITION_TO_SHOW_MODAL';
 export const SHOW_MODAL = 'SHOW_MODAL';
 
 export default function showModal(name, data={}) {
-  return async (dispatch, getState) => {
-    // A modal is already visible, animate it out before showing the current modal
-    if (getState().activeModal.name) {
-      await dispatch(hideModal());
-    }
-
+  return dispatch => {
     // First, ensure the modal is rendered.
     dispatch({ type: TRANSITION_TO_SHOW_MODAL, name, data });
 
