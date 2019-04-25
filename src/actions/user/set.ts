94a5616063687762d78a72e311c2baab96931e7a
Ryan Gaus <rsg1egoman@gmail.com>
2019-04-25 12:34:40 -0400

    code: remove hardcoded size are unit
    Required to test these changes locally

--- src/actions/user/set.ts
+++ src/actions/user/set.ts
@@ -1,7 +1,5 @@
 export const USER_SET = 'USER_SET';
 
 export default function userSet(data) {
-  // TODO: Remove the below
-  data.size_area_unit_default = 'square_meters';
   return { type: USER_SET, data };
 }
