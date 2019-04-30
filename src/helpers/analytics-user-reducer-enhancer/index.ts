db84117544544fb783c701d8aac75fec681a82e7
Brian Weinreich <brian@density.io>
2019-04-30 15:12:44 -0400

    fixed hotjar to not rely on package and instead do it manually

--- src/helpers/analytics-user-reducer-enhancer/index.ts
+++ src/helpers/analytics-user-reducer-enhancer/index.ts
@@ -1,4 +1,5 @@
 import mixpanelInitialize from '../mixpanel-initialize/index';
+import setupHotjar from './hotjar';
 
 
 function isUserImpersonating() {
@@ -9,22 +10,6 @@ function isUserImpersonating() {
     return false;
   }
 }
-
-function setupHotjar(id, sv) {
-  (function(h, o, t, j, a, r) {
-    h.hj =
-      h.hj ||
-      function() {
-        (h.hj.q = h.hj.q || []).push(arguments);
-      };
-    h._hjSettings = { hjid: id, hjsv: sv };
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script');
-    r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
-};
 
 export default function analyticsUserReducerEnhancer(reducer) {
   return (state, props) => {
