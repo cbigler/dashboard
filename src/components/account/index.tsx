7ff1073fdf6adbc3217f1d0dbbf8ed0614c037e7
John Shanley <john.michael.shanley@gmail.com>
2019-05-04 14:31:28 -0400

    Tinkering with the password form

--- src/components/account/index.tsx
+++ src/components/account/index.tsx
@@ -2,8 +2,10 @@ import styles from './styles.module.scss';
 
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
 
 import {
+  Icons,
   InputBox,
   Button,
   Toast,
@@ -18,6 +20,7 @@ import {
 
 import FormLabel from '../form-label/index';
 import ErrorBar from '../error-bar/index';
+import InputBoxInfo from '../input-box-info';
 
 import userResetPassword from '../../actions/user/reset-password';
 import userUpdate from '../../actions/user/update';
@@ -25,17 +28,17 @@ import userUpdate from '../../actions/user/update';
 import showModal from '../../actions/modal/show';
 import hideModal from '../../actions/modal/hide';
 
+import colors from '@density/ui/variables/colors.json';
 
-export const NORMAL = 0;
-export const EDIT = 1;
-export const PASSWORD_RESET = 2;
+// modes for management sections
+const DISPLAY = 'DISPLAY';
+const EDIT = 'EDIT';
 
 
 const GeneralInfoSection = props => {
 
-  const { user, onSubmitUserUpdate } = props;
-
-  const [mode, setMode] = useState(NORMAL);
+  const { user, onSubmitUserUpdate, setErrorText } = props;
+  const [mode, setMode] = useState(DISPLAY);
   
   // this state is only used for editing the user info, not for display (display is always pulled from props)
   const [userFullName, setUserFullName] = useState(user.data.fullName || '');
@@ -46,12 +49,13 @@ const GeneralInfoSection = props => {
 
   const handleCancelButtonClick = evt => {
     setUserFullName(user.data.fullName || '');
-    setMode(NORMAL);
+    setMode(DISPLAY);
   }
 
   const handleSaveButtonClick = evt => {
     onSubmitUserUpdate(userFullName)
-      .then(setMode(NORMAL))
+      .then(setMode(DISPLAY))
+      .catch(err => setErrorText(err))
   }
 
   return (
@@ -60,7 +64,7 @@ const GeneralInfoSection = props => {
         <div className={styles.accountPageSectionHeaderText}>General Info</div>
 
         {/* Edit / Cancel button */}
-        {mode === NORMAL && user.data && !user.data.isDemo ? (
+        {mode === DISPLAY && user.data && !user.data.isDemo ? (
           <Button onClick={handleEditButtonClick}>Edit</Button>
         ) : null}
         {mode === EDIT ? (
@@ -81,8 +85,8 @@ const GeneralInfoSection = props => {
                 type="text"
                 placeholder="Name"
                 width="100%"
-                value={mode === NORMAL ? user.data.fullName : userFullName}
-                onChange={e => setUserFullName(e.target.value)}
+                value={mode === DISPLAY ? user.data.fullName : userFullName}
+                onChange={evt => setUserFullName(evt.target.value)}
                 disabled={mode !== EDIT}
                 id="account-full-name"
               />}
@@ -122,6 +126,62 @@ const GeneralInfoSection = props => {
 }
 
 const PasswordSection = props => {
+
+  const { onSubmitPassword } = props;
+
+  const [mode, setMode] = useState(DISPLAY);
+
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
+
+  const PASSWORD_MIN_LENGTH = 8;
+
+  const [errorText, setErrorText] = useState('');
+
+
+  // possible values for password validation status
+  const VALID = 0
+  const CURRENT_PASSWORD_REQUIRED = 1
+  const PASSWORD_UNCHANGED = 2
+  const NEW_PASSWORD_TOO_SHORT = 3
+  const PASSWORD_CONFIRMATION_MISMATCH = 4
+
+  const currentPasswordExists = currentPassword.length > 0;
+  const passwordHasChanged = newPassword !== currentPassword;
+  const newPasswordIsLongEnough = newPassword.length >= PASSWORD_MIN_LENGTH;
+  const passwordConfirmationMatches = newPassword === newPasswordConfirm;
+
+  let validationStatus = VALID;
+  if (!currentPasswordExists) {
+    validationStatus = CURRENT_PASSWORD_REQUIRED;
+  } else if (!passwordHasChanged) {
+    validationStatus = PASSWORD_UNCHANGED;
+  } else if (!newPasswordIsLongEnough) {
+    validationStatus = NEW_PASSWORD_TOO_SHORT;
+  } else if (!passwordConfirmationMatches) {
+    validationStatus = PASSWORD_CONFIRMATION_MISMATCH;
+  }
+
+  const handleSaveButtonClick = evt => {
+    if (newPassword === newPasswordConfirm) {
+      setErrorText('');
+      return onSubmitPassword(currentPassword, newPassword)
+        .then(() => setMode(DISPLAY))
+        .catch(error => setErrorText(error));
+
+    } else {
+      setErrorText('Passwords don\'t match.');
+    }
+  }
+
+  const handleCancelButtonClick = evt => {
+    setCurrentPassword('');
+    setNewPassword('');
+    setNewPasswordConfirm('');
+    setMode(DISPLAY);
+  }
+
   return (
     <div className={styles.accountPageSection}>
       <div className={styles.accountPageSectionHeader}>
@@ -134,66 +194,91 @@ const PasswordSection = props => {
             <p>Update your super-secret password</p>
           </div>
           <div className={styles.accountPageSectionPopoutForm}>
+            
             {/* Trigger changing the password */}
-            {this.state.mode === NORMAL && canChangePassword ? <FormLabel
-              className={styles.accountChangePasswordLinkContainer}
-              label="Password"
-              htmlFor="account-change-password"
-              input={ <div id="account-change-password" className={styles.accountChangePasswordValue}>
-                <span onClick={() => this.setState({mode: PASSWORD_RESET})}>Change Password</span>
-              </div>}
-            /> : null}
+            {mode === DISPLAY ? (
+              <div
+                className={styles.accountPageSectionPopoutFormTrigger}
+                onClick={evt => setMode(EDIT)}
+              >Change Password</div>
+            ) : null}
 
             {/* The form to change the password that is triggered. */}
-            {this.state.mode === PASSWORD_RESET || true ? <div className={styles.accountChangePasswordFormContainer}>
-              <label className={styles.accountChangePasswordFormHeader}>Password</label>
-              <div className={styles.accountChangePasswordFormFieldWrapper}>
-                <InputBox
-                  type="password"
-                  placeholder="Type old password"
-                  width="100%"
-                  value={this.state.currentPassword}
-                  onChange={e => this.setState({currentPassword: e.target.value})}
-                />
+            {mode === EDIT ? (
+              <div className={styles.accountPageSectionPopoutFormContent}>
+                <div className={styles.passwordForm}>
+                  <div className={styles.passwordFormFieldContainer}>
+                    <FormLabel
+                      htmlFor="account-password-current"
+                      label="Current Password"
+                      input={<InputBox
+                        id="account-password-current"
+                        type="password"
+                        placeholder="Enter your current password"
+                        width="100%"
+                        value={currentPassword}
+                        leftIcon={validationStatus === CURRENT_PASSWORD_REQUIRED ? (
+                          <InputBoxInfo color={colors.brandWarning}>Field is required</InputBoxInfo>
+                        ) : null}
+                        onChange={evt => setCurrentPassword(evt.target.value)}
+                      />}
+                    />
+                    
+                  </div>
+                  <div className={styles.passwordFormFieldContainer}>
+                    <FormLabel
+                      htmlFor="account-password-new"
+                      label="New Password"
+                      input={<InputBox
+                        id="account-password-new"
+                        type="password"
+                        autocomplete="new-password"
+                        placeholder="Enter a new password"
+                        width="100%"
+                        value={newPassword}
+                        leftIcon={validationStatus === NEW_PASSWORD_TOO_SHORT ? (
+                          <InputBoxInfo color={colors.brandWarning}>Must be at least {PASSWORD_MIN_LENGTH} characters</InputBoxInfo>
+                        ) : validationStatus === PASSWORD_UNCHANGED ? (
+                          <InputBoxInfo color={colors.brandDanger}>Must be a new password</InputBoxInfo>
+                        ) : null}
+                        onChange={evt => setNewPassword(evt.target.value)}
+                      />}
+                    />
+                  </div>
+                  <div className={styles.passwordFormFieldContainer}>
+                    <FormLabel
+                      htmlFor="account-password-new-confirm"
+                      label="Confirm New Password"
+                      input={<InputBox
+                        id="account-password-new-confirm"
+                        type="password"
+                        autocomplete="new-password"
+                        placeholder="Confirm your new password"
+                        width="100%"
+                        value={newPasswordConfirm}
+                        leftIcon={validationStatus === PASSWORD_CONFIRMATION_MISMATCH ? (
+                          <InputBoxInfo color={colors.brandDanger}>Password must match</InputBoxInfo>
+                        ) : null}
+                        onChange={evt => setNewPasswordConfirm(evt.target.value)}
+                      />}
+                    />
+                  </div>
+
+                  <div className={styles.passwordFormActions}>
+                    <div className={styles.passwordFormAction}>
+                      <Button onClick={handleCancelButtonClick}>Cancel</Button>
+                    </div>
+                    <div className={styles.passwordFormAction}>
+                    <Button
+                      type="primary"
+                      onClick={handleSaveButtonClick}
+                      disabled={validationStatus !== VALID}
+                    >Change Password</Button>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={styles.accountChangePasswordFormFieldWrapper}>
-                <InputBox
-                  type="password"
-                  placeholder="Type new password (minimum of 8 characters)"
-                  width="100%"
-                  value={this.state.password}
-                  onChange={e => this.setState({password: e.target.value})}
-                />
-              </div>
-              <div className={styles.accountChangePasswordFormFieldWrapper}>
-                <InputBox
-                  type="password"
-                  placeholder="Confirm new password"
-                  width="100%"
-                  value={this.state.passwordConfirmation}
-                  onChange={e => this.setState({passwordConfirmation: e.target.value})}
-                />
-              </div>
-              <Button
-                type="primary"
-                width="100%"
-                onClick={() => {
-                  if (this.state.password === this.state.passwordConfirmation) {
-                    this.setState({error: null});
-                    return onSubmitPassword(this.state.currentPassword, this.state.password)
-                      .then(() => {
-                        this.setState({mode: NORMAL});
-                      })
-                      .catch(error => {
-                        this.setState({ error });
-                      });
-                  } else {
-                    this.setState({error: `Passwords don't match.`});
-                  }
-                }}
-                disabled={this.state.password.length < 8}
-              >Change Password</Button>
-            </div> : null}
+            ) : null}
           </div>
         </div>    
       </div>
@@ -205,26 +290,12 @@ export class Account extends React.Component<any, any> {
   constructor(props) {
     super(props);
     this.state = {
-      mode: NORMAL,
-
       error: null,
-      password: '',
-      currentPassword: '',
-      passwordConfirmation: '',
-
-      // Initialize with a prop passing the initial value from the store
-      fullName: this.props.user.data ? this.props.user.data.fullName : '',
-      email: this.props.user.data ? this.props.user.data.email : '',
-      marketingConsent: this.props.user.data ? this.props.user.data.marketingConsent : false,
     };
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      fullName: nextProps.user.data.fullName || '',
-      email: nextProps.user.data.email || '',
-      marketingConsent: nextProps.user.data.marketingConsent,
-    });
+  setErrorText = text => {
+    this.setState({error: text});
   }
 
   render() {
@@ -234,8 +305,8 @@ export class Account extends React.Component<any, any> {
       onSubmitUserUpdate,
     } = this.props;
 
-    const canChangePassword = user.data && !user.data.isDemo && !user.data.organization.forceSsoLogin;
-
+    // const canChangePassword = user.data && !user.data.isDemo && !user.data.organization.forceSsoLogin;
+    const canChangePassword = true;
     return (
       <AppFrame>
         <AppPane>
@@ -255,7 +326,7 @@ export class Account extends React.Component<any, any> {
               
               {/* PASSWORD */}
               {canChangePassword ? (
-                <PasswordSection />
+                <PasswordSection onSubmitPassword={onSubmitPassword} setErrorText={this.setErrorText} />
               ): null}
               
               <div className={styles.accountDeactivateContainer}>
---
+++ src/components/input-box-info/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+import {
+  Icons,
+} from '@density/ui';
+
+export default function InputBoxInfo({
+  children,
+  color,
+}) {
+  return (
+    <div className={styles.inputBoxInfo}>
+      <div className={styles.inputBoxInfoTrigger}>
+        <Icons.Info color={color}></Icons.Info>
+      </div>
+      <div className={styles.inputBoxInfoContent}>
+        {children}
+      </div>
+    </div>
+  );
+}
