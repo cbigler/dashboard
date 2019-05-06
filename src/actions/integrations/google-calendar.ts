fecbd24aa21d7f3a6adaa537fb1939e0c6e88ee7
Canute Bigler <canute@cbigler.com>
2019-05-06 11:17:13 -0400

    Add failure handling for GCal redirect API call

--- src/actions/integrations/google-calendar.ts
+++ src/actions/integrations/google-calendar.ts
@@ -2,5 +2,7 @@ import core from '../../client/core';
 
 
 export default function doGoogleCalendarAuthRedirect() {
-    return core().get('/integrations/google_calendar/').then(resp => window.location.href = resp.data);
+    return core().get('/integrations/google_calendar/')
+        .then(resp => window.location.href = resp.data)
+        .catch(() => window.location.href = '/#/admin/integrations/google-calendar/fail');
 }
