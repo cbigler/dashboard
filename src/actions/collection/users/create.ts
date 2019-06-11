0051d263492f7dbf848edcd67b343c7713e6869c
Gus Cost <guscost@gmail.com>
2019-06-11 14:42:18 -0400

    code: better error message for duplicate user

--- src/actions/collection/users/create.ts
+++ src/actions/collection/users/create.ts
@@ -2,6 +2,7 @@ import collectionUsersPush from './push';
 import showToast from '../../toasts';
 import mixpanelTrack from '../../../helpers/mixpanel-track/index';
 import accounts from '../../../client/accounts';
+import getBackendErrorDetail from '../../../helpers/get-backend-error-detail';
 
 export const COLLECTION_USERS_CREATE = 'COLLECTION_USERS_CREATE';
 
@@ -19,10 +20,20 @@ export default function collectionUsersCreate(item) {
     } catch (err) {
       // Don't store this error in the error collection for the space, since we are showing a toast
       // for it instead.
-      dispatch(showToast({
-        text: 'Error creating user',
-        type: 'error',
-      }));
+      if (
+        err.response.status === 400 &&
+        getBackendErrorDetail(err) === 'User with this email already invited'
+      ) {
+        dispatch(showToast({
+          text: 'User with this email already exists',
+          type: 'error',
+        }));
+      } else {
+        dispatch(showToast({
+          text: 'Error creating user',
+          type: 'error',
+        }));
+      }
       return false;
     }
 
