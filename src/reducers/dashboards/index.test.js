a2727de2081aec115c0fe4723eda61ecea2998b0
Ryan Gaus <rsg1egoman@gmail.com>
2019-06-20 16:55:01 -0400

    code(test): add a few tests for the important new dashboard actions

--- src/reducers/dashboards/index.test.js
+++ src/reducers/dashboards/index.test.js
@@ -4,10 +4,15 @@ import thunk from 'redux-thunk';
 import assert from 'assert';
 import dashboards from './index';
 
-import collectionDashboardsSet from '../../actions/dashboards/set';
-import collectionDashboardsSelect from '../../actions/dashboards/select';
+import dashboardsSet from '../../actions/dashboards/set';
+import dashboardsSelect from '../../actions/dashboards/select';
+import dashboardsSetData from '../../actions/dashboards/set-data';
+import dashboardsUpdateFormState from '../../actions/dashboards/update-form-state';
+import {
+  clearReportData as dashboardsCalculateReportDataClear,
+} from '../../actions/dashboards/calculate-report-data';
 
-const DASHBOARDS = {
+const DASHBOARD = {
   id: 'das_123',
   name: 'My Dashboard',
   reportSet: [
@@ -34,10 +39,22 @@ const DASHBOARDS = {
   ],
 };
 
+const REPORT_LIST = [
+  { id: 'rpt_xxx', name: 'Sample Report', settings: {}, type: 'SAMPLE_REPORT'},
+];
+
+const TIME_SEGMENT_LABELS = [
+  "foo",
+  "bar",
+  "baz",
+];
+
+const INITIAL_STATE = dashboards(undefined, {});
+
 describe('dashboards', () => {
-  it('should work', async () => {
+  it('should load some report data from a dashboard', async () => {
     const store = createStore(dashboards, {}, applyMiddleware(thunk));
-    store.dispatch(collectionDashboardsSet([DASHBOARDS]));
+    store.dispatch(dashboardsSet([DASHBOARD]));
 
     // Ensure that the now that the dashboard was set, all reports within it are in a loading state
     const loadedState = store.getState();
@@ -45,7 +62,7 @@ describe('dashboards', () => {
     assert.equal(loadedState.calculatedReportData['rpt_456'].state, 'LOADING');
     assert.equal(loadedState.calculatedReportData['rpt_789'].state, 'LOADING');
 
-    await store.dispatch(collectionDashboardsSelect(DASHBOARDS, '2019-01-01', 'Sunday'))
+    await store.dispatch(dashboardsSelect(DASHBOARD, '2019-01-01', 'Sunday'))
 
     const finalState = store.getState();
 
@@ -61,5 +78,41 @@ describe('dashboards', () => {
       'Error was thrown during calculation',
     );
     assert.equal(finalState.calculatedReportData['rpt_789'].data, null);
+  });
+  it('should update the store with data', () => {
+    const result = dashboards(
+      INITIAL_STATE,
+      dashboardsSetData(DASHBOARD, REPORT_LIST, TIME_SEGMENT_LABELS),
+    );
+
+    // Ensure data is set
+    assert.deepStrictEqual(result.data, [DASHBOARD]);
+    assert.deepStrictEqual(result.reportList, REPORT_LIST);
+    assert.deepStrictEqual(result.timeSegmentLabels, TIME_SEGMENT_LABELS);
+
+    // Ensure formstate defaults to the dashboard to be edited
+    assert.deepStrictEqual(result.formState, DASHBOARD);
+
+    // Ensure that all reports are put into a loading state
+    assert.deepStrictEqual(result.calculatedReportData, {
+      'rpt_456': { state: 'LOADING', data: null },
+      'rpt_789': { state: 'LOADING', data: null },
+    });
+  });
+  it('should update the form state when dashboard data is changed on the form', () => {
+    const result = dashboards(INITIAL_STATE, dashboardsUpdateFormState('foo', 'bar'));
+    assert.strictEqual(result.formState.foo, 'bar');
+  });
+  it(`should reset a report's stored data back to being loading`, () => {
+    const initialStateWithReport = {
+      ...INITIAL_STATE,
+      calculatedReportData: {
+        'rpt_123': { state: 'COMPLETE', data: { foo: 'bar' } },
+        'rpt_456': { state: 'COMPLETE', data: { hello: 'world' } },
+      },
+    };
+    const result = dashboards(initialStateWithReport, dashboardsCalculateReportDataClear('rpt_123'));
+    assert.strictEqual(result.calculatedReportData['rpt_123'].state, 'LOADING');
+    assert.strictEqual(result.calculatedReportData['rpt_456'].state, 'COMPLETE');
   });
 });
