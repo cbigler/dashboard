75b420602371b44ff8ebf4449c7a7dbb2527c48c
Ryan Gaus <rsg1egoman@gmail.com>
2019-07-26 15:47:49 -0400

    feat(dashboard-report-edit-modal): add markdown editor
    For the text and insights reports, we need the ability to edit the
    text of them as markdown

---
+++ src/components/markdown-editor/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { InputBox } from '@density/ui';
+import styles from './styles.module.scss';
+
+export default function MarkdownEditor(props) {
+  return (
+    <div className={styles.markdownEditorWrapper}>
+      <InputBox type="textarea" height={300} {...props} />
+      <ul className={styles.markdownEditorHints}>
+        <li><strong>**bold**</strong></li>
+        <li><em>_italics_</em></li>
+        <li><span className={styles.red}>red{'{'}colored text{'}'}</span></li>
+        <li><span className={styles.link}>[link text](http://example.com)</span></li>
+      </ul>
+    </div>
+  );
+}
+
