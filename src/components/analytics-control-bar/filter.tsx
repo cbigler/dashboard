cf69e2c7f4f303a16cba0d367a7106878d2785db
Ryan Gaus <rsg1egoman@gmail.com>
2019-08-15 08:52:20 -0400

    refactor: adjust how filter component works

--- src/components/analytics-control-bar/filter.tsx
+++ src/components/analytics-control-bar/filter.tsx
@@ -1,25 +1,26 @@
-import React, { Fragment, useState, useRef } from 'react';
+import React, { Fragment, useState, useRef, useEffect } from 'react';
 import styles from './filter.module.scss';
 import classnames from 'classnames';
 
+function isElementInside(parentElement, element) {
+  while (element) {
+    if (element === parentElement) {
+      return true;
+    }
+    element = element.parentElement;
+  }
+  return false;
+}
+
 export default function Filter({text, children, open, onOpen, onClose}) {
   const wrapperRef = useRef();
-
-  // This `focusCount` value is used to keep track of if each focus event has a corresponding blur
-  // event within the popup. If not, the popup is closed.
-  const focusCount = useRef();
-
-  function onResetAndClose() {
-    focusCount.current = 1;
-    onClose();
-  }
 
   return (
     <Fragment>
       {open ? (
         <div
           className={styles.backdrop}
-          onClick={onResetAndClose}
+          onClick={onClose}
         />
       ) : null}
 
@@ -28,31 +29,24 @@ export default function Filter({text, children, open, onOpen, onClose}) {
         className={styles.wrapper}
         tabIndex={0}
         onFocus={e => {
-          onOpen();
-
-          focusCount.current = focusCount.current || 0;
-          if (focusCount.current >= 1 && e.target === wrapperRef.current) { return; }
-
-          // For each focus event, add one to the focus count.
-          focusCount.current += 1;
+          const focusedTarget = e.target;
+          const bluredTarget = e.relatedTarget;
+          if (
+            isElementInside(wrapperRef.current, focusedTarget) &&
+            !isElementInside(wrapperRef.current, bluredTarget)
+          ) {
+            onOpen();
+          }
         }}
         onBlur={e => {
-          // There's a bit of trickyness going on here. Delay this blur event slightly so that it
-          // fires after the corresponding focus event.
-          let target = e.target;
-          setTimeout(() => {
-
-            // If the focus count is odd, there was a focus that happened outside of the popup.
-            // Close the popup.
-            if (focusCount.current % 2 === 1) {
-              onResetAndClose();
-              return;
-            } else {
-              // For each blur event, remove one from the focus count. If each focus has a correspondign
-              // blue event, then the focus count should be 0.
-              focusCount.current -= 1;
-            }
-          }, 50)
+          const bluredTarget = e.target;
+          const focusedTarget = e.relatedTarget;
+          if (
+            !isElementInside(wrapperRef.current, focusedTarget) &&
+            isElementInside(wrapperRef.current, bluredTarget)
+          ) {
+            onClose();
+          }
         }}
       >
         {/* Filter Label */}
--- src/components/analytics-control-bar/index.tsx
+++ src/components/analytics-control-bar/index.tsx
@@ -2,6 +2,10 @@ import React, { useState, Fragment } from 'react';
 import styles from './styles.module.scss';
 import classnames from 'classnames';
 
+// FIXME: move this
+import { SPACE_FUNCTION_CHOICES } from '../admin-locations-detail-modules/general-info';
+// FIXME: move this
+
 import Filter, { FilterBold } from './filter';
 
 import {
@@ -10,6 +14,7 @@ import {
   AppBarTitle,
   AppBarContext,
   Icons,
+  InputBox,
 } from '@density/ui';
 
 export default function AnalyticsControlBar() {
@@ -24,7 +29,15 @@ function ItemList({choices, onClick}) {
   return (
     <ul className={styles.itemList}>
       {choices.map(choice => (
-        <li tabIndex={0} onClick={() => onClick(choice)}>
+        <li
+          tabIndex={0}
+          onClick={() => onClick(choice)}
+          onKeyPress={e => {
+            if (e.key === 'Enter') {
+              onClick(choice);
+            }
+          }}
+        >
           {choice.label}
         </li>
       ))}
@@ -52,44 +65,103 @@ enum AnalyticsSpaceSelectorPage {
   SpaceType = 'SpaceType',
 }
 
+const ANALYTICS_PAGE_TO_LABEL = {
+  [AnalyticsSpaceSelectorPage.SpaceFunction]: 'Add by Function',
+  [AnalyticsSpaceSelectorPage.SpaceType]: 'Add by Type',
+  [AnalyticsSpaceSelectorPage.SpaceName]: 'Add by Space Name',
+};
+
 export function AnalyticsSpaceSelector({ spaces, onChange }: AnalyticsSpaceSelectorProps) {
   const [ open, setOpen ] = useState(false);
   const [ page, setPage ] = useState(AnalyticsSpaceSelectorPage.List);
+  const [ searchText, setSearchText ] = useState('');
+
+  function onCloseAndReset() {
+    setOpen(false);
+    setTimeout(() => {
+      setPage(AnalyticsSpaceSelectorPage.List);
+      setSearchText('');
+    }, 250);
+  };
+
   return (
     <Filter
       open={open}
       onOpen={() => setOpen(true)}
-      onClose={() => {
-        setOpen(false);
-        setTimeout(() => {
-          setPage(AnalyticsSpaceSelectorPage.List);
-        }, 250);
-      }}
+      onClose={onCloseAndReset}
       text={<Fragment><FilterBold>Space</FilterBold> is <FilterBold>Orange</FilterBold></Fragment>}
     >
       {page === AnalyticsSpaceSelectorPage.List ? (
-        <ItemList
-          choices={[
-            {id: AnalyticsSpaceSelectorPage.SpaceFunction, label: 'Add by Function'},
-            {id: AnalyticsSpaceSelectorPage.SpaceType, label: 'Add by Type'},
-            {id: AnalyticsSpaceSelectorPage.SpaceName, label: 'Add by Space Name'},
-          ]}
-          onClick={choice => setPage(choice.id)}
-        />
+        // Show a list of potential filters on the list page
+        <div className={styles.popupBodySmall}>
+          <ItemList
+            choices={
+              Object.entries(ANALYTICS_PAGE_TO_LABEL)
+              .map(([key, value]) => ({id: key, label: value}))
+            }
+            onClick={choice => setPage(choice.id)}
+          />
+        </div>
       ) : (
+        // When not on the list page, show a header that allows navigation back to the list page
         <div className={styles.header}>
           <AppBarContext.Provider value="CARD_HEADER">
             <AppBar>
               <AppBarTitle>
-                <div role="button" tabIndex={0} className={styles.back} onClick={() => setPage(AnalyticsSpaceSelectorPage.List)}>
+                <div
+                  role="button"
+                  tabIndex={0}
+                  className={styles.back}
+                  onClick={() => setPage(AnalyticsSpaceSelectorPage.List)}
+                >
                   <Icons.ArrowLeft />
                 </div>
-                Foo
+                {ANALYTICS_PAGE_TO_LABEL[page]}
               </AppBarTitle>
             </AppBar>
           </AppBarContext.Provider>
         </div>
       )}
+
+      {page === AnalyticsSpaceSelectorPage.SpaceFunction ? (
+        <Fragment>
+          <div className={styles.popupBody}>
+            <ItemList
+              choices={SPACE_FUNCTION_CHOICES}
+              onClick={choice => {
+                onCloseAndReset();
+                console.log(choice);
+              }}
+            />
+          </div>
+        </Fragment>
+      ) : null}
+
+      {page === AnalyticsSpaceSelectorPage.SpaceType ? (
+        <Fragment>
+          <AppBar>
+            <InputBox
+              type="text"
+              placeholder="Search for a space type"
+              width="100%"
+            />
+          </AppBar>
+          <div className={styles.popupBody}>
+            <ItemList
+              choices={[
+                {id: 'space', label: 'Campus'},
+                {id: 'space', label: 'Building'},
+                {id: 'space', label: 'Floor'},
+                {id: 'space', label: 'Space'},
+              ]}
+              onClick={choice => {
+                onCloseAndReset();
+                console.log(choice);
+              }}
+            />
+          </div>
+        </Fragment>
+      ) : null}
     </Filter>
   );
 }
