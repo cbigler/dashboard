2a87b1f21733370a524323a17ef87f5d1d1aee1c
Ryan Gaus <rsg1egoman@gmail.com>
2019-08-21 15:11:13 -0400

    refactor(analytics-control-bar): extract custom range page to its own component

--- src/components/analytics-control-bar/index.tsx
+++ src/components/analytics-control-bar/index.tsx
@@ -636,6 +636,137 @@ enum AnalyticsCustomDateRangeChoices {
   ON = 'on',
 }
 
+const BETWEEN_DEFAULT_RANGE: DateRange = {
+  type: RangeType.ABSOLUTE,
+  startDate: moment().subtract(1, 'week').startOf('week').format('YYYY-MM-DD'),
+  endDate: moment().subtract(1, 'week').endOf('week').format('YYYY-MM-DD'),
+};
+
+const ON_DEFAULT_RANGE: DateRange = {
+  type: RangeType.ABSOLUTE,
+  startDate: moment().format('YYYY-MM-DD'),
+  endDate: moment().format('YYYY-MM-DD'),
+};
+
+function DateSelectorCustom({ workingDateRange, setWorkingDateRange, onSubmit }) {
+  const [ dateRangeFocused, setDateRangeFocused ] = useState('startDate');
+  const [ customDateRangeType, setCustomDateRangeType ] = useState(AnalyticsCustomDateRangeChoices.BETWEEN);
+
+  useEffect(() => {
+    // Figure out the custom date range type if the custom page is visible
+    if (workingDateRange && workingDateRange.type === RangeType.ABSOLUTE) {
+      setCustomDateRangeType(
+        workingDateRange.startDate === workingDateRange.endDate ?
+          AnalyticsCustomDateRangeChoices.ON :
+          AnalyticsCustomDateRangeChoices.BETWEEN
+      );
+    }
+  }, [ workingDateRange ]);
+
+  return (
+    <div className={styles.dateSelectorCustomWrapper}>
+      <div className={styles.header}>
+        <AppBarContext.Provider value="CARD_HEADER">
+          <AppBar>
+            <AppBarTitle>
+              <div className={styles.back}>
+                <BackButton onClick={() => setWorkingDateRange(null)} />
+              </div>
+              Select a Custom Date Range
+            </AppBarTitle>
+          </AppBar>
+        </AppBarContext.Provider>
+      </div>
+      <div className={styles.dateSelectorCustomBody}>
+        <div className={styles.dateSelectorCustomBodyItem}>
+          <InputBox
+            type="select"
+            value={customDateRangeType}
+            choices={[
+              { id: AnalyticsCustomDateRangeChoices.BETWEEN, label: 'Between' },
+              { id: AnalyticsCustomDateRangeChoices.ON, label: 'On' },
+            ]}
+            onChange={choice => {
+              setCustomDateRangeType(choice.id);
+
+              switch (choice.id) {
+              case AnalyticsCustomDateRangeChoices.BETWEEN:
+                setWorkingDateRange(BETWEEN_DEFAULT_RANGE);
+                break;
+              case AnalyticsCustomDateRangeChoices.ON:
+                setWorkingDateRange(ON_DEFAULT_RANGE);
+                break;
+              }
+            }}
+            width="100%"
+          />
+        </div>
+        <div
+          className={styles.dateSelectorCustomBodyItem}
+          style={{flexGrow: 1, flexShrink: 1}}
+
+          // Block focus / blur events from effecting the filter popup logic
+          onFocus={e => e.stopPropagation()}
+          onBlur={e => e.stopPropagation()}
+        >
+          {customDateRangeType === AnalyticsCustomDateRangeChoices.BETWEEN ? (
+            <DateRangePicker
+              focusedInput={dateRangeFocused}
+              onFocusChange={focus => {
+                if (focus !== null) {
+                  setDateRangeFocused(focus);
+                }
+              }}
+              startDate={workingDateRange.startDate ? moment(workingDateRange.startDate, 'YYYY-MM-DD') : null}
+              endDate={workingDateRange.endDate ? moment(workingDateRange.endDate, 'YYYY-MM-DD') : null}
+              onChange={range => {
+                if (!range.endDate) {
+                  // A date before the start of the selected range was clicked, so use the date
+                  // that was selected 
+                  setWorkingDateRange({
+                    ...workingDateRange,
+                    startDate: range.startDate.format('YYYY-MM-DD'),
+                    endDate: '',
+                  });
+                } else {
+                  setWorkingDateRange({
+                    ...workingDateRange,
+                    startDate: range.startDate.format('YYYY-MM-DD'),
+                    endDate: range.endDate.format('YYYY-MM-DD'),
+                  });
+                }
+              }}
+              numberOfMonths={1}
+              isOutsideRange={day => day.clone().startOf('day').isAfter(moment().startOf('day'))}
+            />
+          ) : (
+            <DatePicker
+              focused
+              onFocusChange={() => {}}
+              date={workingDateRange.startDate ? moment(workingDateRange.startDate, 'YYYY-MM-DD') : null}
+              onChange={date => {
+                setWorkingDateRange({
+                  ...workingDateRange,
+                  startDate: date.format('YYYY-MM-DD'),
+                  endDate: date.format('YYYY-MM-DD'),
+                });
+              }}
+              numberOfMonths={1}
+              isOutsideRange={day => day.clone().startOf('day').isAfter(moment().startOf('day'))}
+            />
+          )}
+        </div>
+      </div>
+      <SubmitButton
+        onClick={() => onSubmit(workingDateRange)}
+        disabled={!workingDateRange.startDate || !workingDateRange.endDate}
+      >
+        Select date range
+      </SubmitButton>
+    </div>
+  );
+}
+
 const TIMEFRAME_CHOICES: Array<DateRange> = [
   DATE_RANGES.TODAY,
   DATE_RANGES.WEEK_TO_DATE,
@@ -655,34 +786,17 @@ export function AnalyticsDateSelector({ value, onChange }) {
     text = value.label || '(unlabeled relative date range)';
     break;
   case RangeType.ABSOLUTE:
-    text = `${moment.utc(value.startDate).format('MM/DD/YYYY')} - ${moment.utc(value.endDate).format('MM/DD/YYYY')}`;
+    const start = moment.utc(value.startDate).format('MM/DD/YYYY');
+    const end = moment.utc(value.endDate).format('MM/DD/YYYY');
+    if (start === end) {
+      text = start;
+    } else {
+      text = `${start} - ${end}`;
+    }
     break;
   }
 
   const [ workingDateRange, setWorkingDateRange ] = useState(TIMEFRAME_CHOICES[0]);
-  const [ customRangeDatePickerFocus, setCustomRangeDatePickerFocus ] = useState('startDate');
-  const [ customDateRangeType, setCustomDateRangeType ] = useState(AnalyticsCustomDateRangeChoices.BETWEEN);
-
-  function onOpen() {
-    if (open) { return; }
-    setWorkingDateRange(value);
-    setCustomRangeDatePickerFocus('startDate');
-
-    // Figure out the custom date range type if the custom page is visible
-    if (workingDateRange && workingDateRange.type === RangeType.ABSOLUTE) {
-      if (workingDateRange.startDate === '' && workingDateRange.endDate === '') {
-        setCustomDateRangeType(AnalyticsCustomDateRangeChoices.BETWEEN);
-      } else {
-        setCustomDateRangeType(
-          workingDateRange.startDate === workingDateRange.endDate ?
-            AnalyticsCustomDateRangeChoices.ON :
-            AnalyticsCustomDateRangeChoices.BETWEEN
-        );
-      }
-    }
-
-    setOpen(true);
-  }
 
   let activePage;
   if (workingDateRange && workingDateRange.type === RangeType.ABSOLUTE) {
@@ -692,145 +806,59 @@ export function AnalyticsDateSelector({ value, onChange }) {
   }
 
   return (
-    <Filter
-      open={open}
-      onOpen={() => onOpen()}
-      onClose={() => setOpen(false)}
-      text={<FilterBold>{text}</FilterBold>}
-    >
-      {activePage === AnalyticsDateSelectorPages.LIST ? (
-        <ItemList
-          template={ARROW_TEMPLATE}
-          choices={[
-            ...TIMEFRAME_CHOICES.map(choice => ({id: choice.label, label: choice.label})),
-            {id: 'ABSOLUTE', label: 'Custom Date Range'},
-          ]}
-          onClick={choice => {
-            const timeRange = TIMEFRAME_CHOICES.find(c => c.label === choice.id);
-            if (timeRange) {
-              // An item in the regular list was picked
-              onChange(timeRange);
-              setOpen(false);
-            } else if (choice.id === 'ABSOLUTE') {
-              // The "custom date range" choice was picked
-              setWorkingDateRange({
-                type: RangeType.ABSOLUTE,
-                startDate: moment().subtract(1, 'week').startOf('week').format('YYYY-MM-DD'),
-                endDate: moment().subtract(1, 'week').endOf('week').format('YYYY-MM-DD'),
-              });
-            }
-          }}
-        />
-      ) : null}
-      {activePage === AnalyticsDateSelectorPages.CUSTOM ? (
-        <div className={styles.dateSelectorCustomWrapper}>
-          <div className={styles.header}>
-            <AppBarContext.Provider value="CARD_HEADER">
-              <AppBar>
-                <AppBarTitle>
-                  <div className={styles.back}>
-                    <BackButton onClick={() => setWorkingDateRange(null)} />
-                  </div>
-                  Select a Custom Date Range
-                </AppBarTitle>
-              </AppBar>
-            </AppBarContext.Provider>
-          </div>
-          <div className={styles.dateSelectorCustomBody}>
-            <div className={styles.dateSelectorCustomBodyItem}>
-              <InputBox
-                type="select"
-                value={customDateRangeType}
-                choices={[
-                  { id: AnalyticsCustomDateRangeChoices.BETWEEN, label: 'Between' },
-                  { id: AnalyticsCustomDateRangeChoices.ON, label: 'On' },
-                ]}
-                onChange={choice => {
-                  switch (choice.id) {
-                  case AnalyticsCustomDateRangeChoices.BETWEEN:
-                    setWorkingDateRange({
-                      ...workingDateRange,
-                      startDate: moment().subtract(1, 'week').startOf('week').format('YYYY-MM-DD'),
-                      endDate: moment().subtract(1, 'week').endOf('week').format('YYYY-MM-DD'),
-                    });
-                  case AnalyticsCustomDateRangeChoices.ON:
-                    setWorkingDateRange({
-                      ...workingDateRange,
-                      startDate: moment().format('YYYY-MM-DD'),
-                      endDate: moment().format('YYYY-MM-DD'),
-                    });
-                  }
-                  setCustomDateRangeType(choice.id);
-                }}
-                width="100%"
-              />
-            </div>
-            <div
-              className={styles.dateSelectorCustomBodyItem}
-              style={{flexGrow: 1, flexShrink: 1}}
-
-              // Block focus / blur events from effecting the filter popup logic
-              onFocus={e => e.stopPropagation()}
-              onBlur={e => e.stopPropagation()}
-            >
-              {customDateRangeType === AnalyticsCustomDateRangeChoices.BETWEEN ? (
-                <DateRangePicker
-                  focusedInput={customRangeDatePickerFocus}
-                  onFocusChange={focus => {
-                    if (focus !== null) {
-                      setCustomRangeDatePickerFocus(focus);
-                    }
-                  }}
-                  startDate={workingDateRange.startDate ? moment(workingDateRange.startDate, 'YYYY-MM-DD') : null}
-                  endDate={workingDateRange.endDate ? moment(workingDateRange.endDate, 'YYYY-MM-DD') : null}
-                  onChange={range => {
-                    if (!range.endDate) {
-                      // A date before the start of the selected range was clicked, so use the date
-                      // that was selected 
-                      setWorkingDateRange({
-                        ...workingDateRange,
-                        startDate: range.startDate.format('YYYY-MM-DD'),
-                        endDate: '',
-                      });
-                    } else {
-                      setWorkingDateRange({
-                        ...workingDateRange,
-                        startDate: range.startDate.format('YYYY-MM-DD'),
-                        endDate: range.endDate.format('YYYY-MM-DD'),
-                      });
-                    }
-                  }}
-                  numberOfMonths={1}
-                  isOutsideRange={day => day.clone().startOf('day').isAfter(moment().startOf('day'))}
-                />
-              ) : (
-                <DatePicker
-                  focused
-                  date={workingDateRange.startDate ? moment(workingDateRange.startDate, 'YYYY-MM-DD') : null}
-                  onChange={date => {
-                    setWorkingDateRange({
-                      ...workingDateRange,
-                      startDate: date.format('YYYY-MM-DD'),
-                      endDate: date.format('YYYY-MM-DD'),
-                    });
-                  }}
-                  numberOfMonths={1}
-                  isOutsideRange={day => day.clone().startOf('day').isAfter(moment().startOf('day'))}
-                />
-              )}
-            </div>
-          </div>
-          <SubmitButton
-            onClick={() => {
-              onChange(workingDateRange);
+    <div className={styles.dateSelector}>
+      <span className={styles.dateSelectorLabel}>
+        {value && value.type === RangeType.ABSOLUTE ? (
+          value.startDate === value.endDate ? 'on' : 'from'
+        ) : (
+          value && value.label && value.label.startsWith('Last') ? 'in the' : 'from'
+        )}
+      </span>
+      <Filter
+        open={open}
+        onOpen={() => {
+          if (open) { return; }
+          setWorkingDateRange(value);
+          setOpen(true);
+        }}
+        onClose={() => setOpen(false)}
+        text={<FilterBold>{text}</FilterBold>}
+      >
+        {activePage === AnalyticsDateSelectorPages.LIST ? (
+          <ItemList
+            template={ARROW_TEMPLATE}
+            choices={[
+              ...TIMEFRAME_CHOICES.map(choice => ({id: choice.label, label: choice.label})),
+              {id: 'ABSOLUTE', label: 'Custom Date Range'},
+            ]}
+            onClick={choice => {
+              const timeRange = TIMEFRAME_CHOICES.find(c => c.label === choice.id);
+              if (timeRange) {
+                // An item in the regular list was picked
+                onChange(timeRange);
+                setOpen(false);
+              } else if (choice.id === 'ABSOLUTE') {
+                // The "custom date range" choice was picked
+                setWorkingDateRange({
+                  type: RangeType.ABSOLUTE,
+                  startDate: moment().subtract(1, 'week').startOf('week').format('YYYY-MM-DD'),
+                  endDate: moment().subtract(1, 'week').endOf('week').format('YYYY-MM-DD'),
+                });
+              }
+            }}
+          />
+        ) : null}
+        {activePage === AnalyticsDateSelectorPages.CUSTOM ? (
+          <DateSelectorCustom
+            workingDateRange={workingDateRange}
+            setWorkingDateRange={setWorkingDateRange}
+            onSubmit={dateRange => {
+              onChange(dateRange);
               setOpen(false);
             }}
-            disabled={!workingDateRange.startDate || !workingDateRange.endDate}
-          >
-            Select date range
-          </SubmitButton>
-        </div>
-      ) : null}
-    </Filter>
+          />
+        ) : null}
+      </Filter>
+    </div>
   );
 }
