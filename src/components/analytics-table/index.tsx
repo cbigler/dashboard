421cdc3aa2627377e2012fe8558d3ba27f73ef1c
Ryan Gaus <rsg1egoman@gmail.com>
2019-09-10 11:49:33 -0400

    feat(analytics-table): add header row to table
    This header row shows data combined for "all" spaces.

--- src/components/analytics-table/index.tsx
+++ src/components/analytics-table/index.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import classnames from 'classnames';
 import styles from './styles.module.scss';
 import { DensitySpaceTypes, DensitySpace, DensitySpaceCountMetrics } from '../../types';
 import { formatSpaceFunction } from '../../helpers/space-function-choices';
@@ -8,10 +9,13 @@ import analyticsColorScale from '../../helpers/analytics-color-scale';
 import { ascending, descending } from '../../helpers/natural-sorting';
 
 import {
+  Icons,
   ListView,
   ListViewColumn,
+  ListViewColumnSpacer,
   getNextSortDirection,
 } from '@density/ui';
+import colorVariables from '@density/ui/variables/colors.json';
 import Checkbox from '../checkbox';
 
 type AnalyticsDatapoint = {
@@ -107,9 +111,17 @@ type TableDataItem = {
   metricData: DensitySpaceCountMetrics,
 };
 
-type SortDirection = 'asc' | 'desc' | 'none';
+enum SortDirection {
+  ASCENDING = 'asc',
+  DESCENDING = 'desc',
+  NONE = 'none',
+}
+
 type SortColumn = string | null;
 type SortFunction = (a: any, b: any) => number;
+
+const INACTIVE_CHEVRON_COLOR = colorVariables.gray,
+      ACTIVE_CHEVRON_COLOR = colorVariables.grayCinder;
 
 function formatSpaceType(spaceType: DensitySpaceTypes) {
   switch (spaceType) {
@@ -125,6 +137,31 @@ function formatSpaceType(spaceType: DensitySpaceTypes) {
     return spaceType;
   }
 }
+
+function sum(acc: number, n: number): number {
+  return acc + n;
+}
+
+function Header({label, value, spaceSortDirection, spaceSortColumn, right=false}) {
+  const chevronColor = spaceSortColumn === label && spaceSortDirection !== SortDirection.NONE ? ACTIVE_CHEVRON_COLOR : INACTIVE_CHEVRON_COLOR;
+  const chevronFlipped = spaceSortColumn === label && spaceSortDirection === SortDirection.ASCENDING
+  return (
+    <div className={classnames(styles.header, {[styles.right]: right})}>
+      <span className={styles.headerLabel}>
+        {label}
+        {chevronFlipped ? (
+          <Icons.ChevronUp color={chevronColor} />
+        ) : (
+          <Icons.ChevronDown color={chevronColor} />
+        )}
+      </span>
+      <div className={styles.headerFakeRowText}>
+        {value}
+      </div>
+    </div>
+  );
+}
+// {Math.max(...tableData.map(x => x.metricData.count.max.value))}
 
 export default function AnalyticsTable({
   spaces,
@@ -177,7 +214,7 @@ export default function AnalyticsTable({
         <div className={styles.tableView}>
           <ListView
             rowHeight={38}
-            headerHeight={38}
+            headerHeight={56}
             data={tableData}
             sort={[{
               column: spaceSortColumn,
@@ -188,9 +225,9 @@ export default function AnalyticsTable({
               const sortDirection = getNextSortDirection(lastSortDirection);
               const sortFunction = (() => {
                 switch (sortDirection) {
-                  case 'asc':
+                  case SortDirection.ASCENDING:
                     return (a: TableDataItem, b: TableDataItem) => ascending(template(a), template(b))
-                  case 'desc':
+                  case SortDirection.DESCENDING:
                     return (a: TableDataItem, b: TableDataItem) => descending(template(a), template(b))
                   default:
                     return (a:any, b:any) => 0
@@ -200,23 +237,34 @@ export default function AnalyticsTable({
             }}
           >
             <ListViewColumn
-              id="Name"
+              id="Space"
               title={
-                <span className={styles.tableNameHeader}>
-                  <Checkbox
-                    checked={allSpacesVisible}
-                    onChange={e => onChangeHiddenSpaceIds((e.target as HTMLInputElement).checked ? [] : selectedSpaces.map(i => i.id))}
-                  />
-                  <span className={styles.tableNameLabel}>Name</span>
-                </span>
+                <div className={styles.header}>
+                  <span className={styles.tableSpaceHeader}>
+                    <Checkbox
+                      checked={allSpacesVisible}
+                      onChange={e => onChangeHiddenSpaceIds((e.target as HTMLInputElement).checked ? [] : selectedSpaces.map(i => i.id))}
+                    />
+                    <span className={styles.tableSpaceLabel}>
+                      Space
+                      {spaceSortColumn === 'Space' && spaceSortDirection === SortDirection.ASCENDING ? (
+                        <Icons.ChevronUp color={ACTIVE_CHEVRON_COLOR} />
+                      ) : (
+                        <Icons.ChevronDown color={spaceSortColumn === 'Space' && spaceSortDirection === SortDirection.DESCENDING ? ACTIVE_CHEVRON_COLOR : INACTIVE_CHEVRON_COLOR} />
+                      )}
+                    </span>
+                  </span>
+                  <div className={classnames(styles.headerFakeRowText, styles.all)}>
+                    All
+                  </div>
+                </div>
               }
               onClick={(x: TableDataItem) => changeSpaceVisibility(x.space, !x.isVisible)}
-              isRowHeader={true}
               width={240}
               valueTemplate={(x: TableDataItem) => x.space.name}
               template={(x: TableDataItem) => (
                 <span
-                  className={styles.tableName}
+                  className={styles.tableSpace}
                   style={{ color: analyticsColorScale(x.space) }}
                 >
                   <Checkbox
@@ -227,41 +275,114 @@ export default function AnalyticsTable({
                       changeSpaceVisibility(x.space, visible);
                     }}
                   />
-                  <span className={styles.tableNameLabel}>{x.space.name}</span>
+                  <span className={styles.tableSpaceLabel}>{x.space.name}</span>
                 </span>
               )}
             />
             <ListViewColumn
+              id="Location"
+              width={400}
+              title={
+                <Header
+                  label="Location"
+                  value="---"
+                  spaceSortDirection={spaceSortDirection}
+                  spaceSortColumn={spaceSortColumn}
+                />
+              }
+              template={(x: TableDataItem) => x.space.ancestry.map(i => i.name).join(' / ')}
+            />
+            <ListViewColumn
               id="Type"
               width={80}
+              title={
+                <Header
+                  label="Type"
+                  value="---"
+                  spaceSortDirection={spaceSortDirection}
+                  spaceSortColumn={spaceSortColumn}
+                />
+              }
               template={(x: TableDataItem) => formatSpaceType(x.space.spaceType)}
             />
             <ListViewColumn
               id="Function"
               width={180}
-              template={(x: TableDataItem) => x.space['function'] ? formatSpaceFunction(x.space['function']) : x.space['function']}
+              title={
+                <Header
+                  label="Function"
+                  value="---"
+                  spaceSortDirection={spaceSortDirection}
+                  spaceSortColumn={spaceSortColumn}
+                />
+              }
+              template={(x: TableDataItem) => x.space['function'] ? formatSpaceFunction(x.space['function']) : <em>N/A</em>}
             />
+            <ListViewColumn
+              id="Capacity"
+              width={180}
+              title={
+                <Header
+                  label="Capacity"
+                  value="---"
+                  spaceSortDirection={spaceSortDirection}
+                  spaceSortColumn={spaceSortColumn}
+                />
+              }
+              template={(x: TableDataItem) => x.space.capacity ? formatMetricNumber(x.space.capacity) : <em>N/A</em>}
+            />
+            <ListViewColumnSpacer />
 
             {/* VISITS */}
             {analyticsReport.selectedMetric === AnalyticsFocusedMetric.ENTRANCES ? (
               <Fragment>
                 <ListViewColumn
-                  id="Total Visits"
+                  id="Max"
+                  align="right"
                   width={110}
-                  valueTemplate={(x: TableDataItem) => x.metricData.entrances.total}
-                  template={(x: TableDataItem) => formatMetricNumber(x.metricData.entrances.total)}
+                  title={
+                    <Header
+                      label="Max"
+                      value={formatMetricNumber(Math.max(...tableData.map(x => x.metricData.count.max.value)))}
+                      spaceSortDirection={spaceSortDirection}
+                      spaceSortColumn={spaceSortColumn}
+                      right
+                    />
+                  }
+                  valueTemplate={(x: TableDataItem) => x.metricData.count.max.value}
+                  template={(x: TableDataItem) => formatMetricNumber(x.metricData.count.max.value)}
                 />
                 <ListViewColumn
-                  id="Avg Visits"
+                  id="Average"
+                  align="right"
                   width={110}
+                  title={
+                    <Header
+                      label="Average"
+                      value={formatMetricNumber(tableData.map(x => x.metricData.entrances.average).reduce(sum) / tableData.length)}
+                      spaceSortDirection={spaceSortDirection}
+                      spaceSortColumn={spaceSortColumn}
+                      right
+                    />
+                  }
                   valueTemplate={(x: TableDataItem) => x.metricData.entrances.average}
                   template={(x: TableDataItem) => formatMetricNumber(x.metricData.entrances.average)}
                 />
                 <ListViewColumn
-                  id="Peak Visits"
+                  id="Total"
                   width={110}
-                  valueTemplate={(x: TableDataItem) => x.metricData.entrances.peak.value}
-                  template={(x: TableDataItem) => formatMetricNumber(x.metricData.entrances.peak.value)}
+                  align="right"
+                  title={
+                    <Header
+                      label="Total"
+                      value={formatMetricNumber(tableData.map(x => x.metricData.entrances.total).reduce(sum))}
+                      spaceSortDirection={spaceSortDirection}
+                      spaceSortColumn={spaceSortColumn}
+                      right
+                    />
+                  }
+                  valueTemplate={(x: TableDataItem) => x.metricData.entrances.total}
+                  template={(x: TableDataItem) => formatMetricNumber(x.metricData.entrances.total)}
                 />
               </Fragment>
             ) : null}
@@ -270,20 +391,34 @@ export default function AnalyticsTable({
             {analyticsReport.selectedMetric === AnalyticsFocusedMetric.MAX ? (
               <Fragment>
                 <ListViewColumn
-                  id="Max Count"
+                  id="Max"
+                  align="right"
                   width={110}
+                  title={
+                    <Header
+                      label="Max"
+                      value={formatMetricNumber(Math.max(...tableData.map(x => x.metricData.count.max.value)))}
+                      spaceSortDirection={spaceSortDirection}
+                      spaceSortColumn={spaceSortColumn}
+                      right
+                    />
+                  }
                   valueTemplate={(x: TableDataItem) => x.metricData.count.max.value}
                   template={(x: TableDataItem) => formatMetricNumber(x.metricData.count.max.value)}
                 />
                 <ListViewColumn
-                  id="Min Count"
+                  id="Average"
+                  align="right"
                   width={110}
-                  valueTemplate={(x: TableDataItem) => x.metricData.count.min.value}
-                  template={(x: TableDataItem) => formatMetricNumber(x.metricData.count.min.value)}
-                />
-                <ListViewColumn
-                  id="Avg Count"
-                  width={110}
+                  title={
+                    <Header
+                      label="Average"
+                      value={formatMetricNumber(tableData.map(x => x.metricData.count.average).reduce(sum) / tableData.length)}
+                      spaceSortDirection={spaceSortDirection}
+                      spaceSortColumn={spaceSortColumn}
+                      right
+                    />
+                  }
                   valueTemplate={(x: TableDataItem) => x.metricData.count.average}
                   template={(x: TableDataItem) => formatMetricNumber(x.metricData.count.average)}
                 />
@@ -294,20 +429,41 @@ export default function AnalyticsTable({
             {analyticsReport.selectedMetric === AnalyticsFocusedMetric.UTILIZATION ? (
               <Fragment>
                 <ListViewColumn
-                  id="Max Utilization"
+                  id="Max"
+                  title={
+                    <Header
+                      label="Max"
+                      value={<Fragment>
+                        {formatMetricNumber(Math.max(...tableData.map(x => x.metricData.targetUtilization.max.value)))}%
+                      </Fragment>}
+                      spaceSortDirection={spaceSortDirection}
+                      spaceSortColumn={spaceSortColumn}
+                      right
+                    />
+                  }
                   width={130}
+                  align="right"
                   valueTemplate={(x: TableDataItem) => x.metricData.targetUtilization.max.value}
-                  template={(x: TableDataItem) => <span>{formatMetricNumber((x.metricData.targetUtilization.max || {}).value)}%</span>} />
+                  template={(x: TableDataItem) => <span>{formatMetricNumber((x.metricData.targetUtilization.max || {}).value)}%</span>}
+                />
                 <ListViewColumn
-                  id="Min Utilization"
-                  width={130}
-                  valueTemplate={(x: TableDataItem) => x.metricData.targetUtilization.min.value}
-                  template={(x: TableDataItem) => <span>{formatMetricNumber((x.metricData.targetUtilization.min || {}).value)}%</span>} />
-                <ListViewColumn
-                  id="Avg Utilization"
+                  id="Average"
+                  title={
+                    <Header
+                      label="Average"
+                      value={<Fragment>
+                        {formatMetricNumber(tableData.map(x => x.metricData.targetUtilization.average).reduce(sum) / tableData.length)}%
+                      </Fragment>}
+                      spaceSortDirection={spaceSortDirection}
+                      spaceSortColumn={spaceSortColumn}
+                      right
+                    />
+                  }
+                  align="right"
                   width={130}
                   valueTemplate={(x: TableDataItem) => x.metricData.targetUtilization.average}
-                  template={(x: TableDataItem) => <span>{formatMetricNumber(x.metricData.targetUtilization.average)}%</span>} />
+                  template={(x: TableDataItem) => <Fragment>{formatMetricNumber(x.metricData.targetUtilization.average)}%</Fragment>}
+                />
               </Fragment>
             ) : null}
           </ListView>
--- src/components/analytics-table/story.tsx
+++ src/components/analytics-table/story.tsx
@@ -19,11 +19,15 @@ function State({ initialState, children }) {
       <br />
       <strong>State:</strong>
       <br />
-      <pre style={{height: 100, overflow: 'auto', padding: 8, background: '#eee'}}>
+      <pre style={{height: 300, overflow: 'auto', padding: 8, background: '#eee'}}>
         {JSON.stringify(state, null, 2)}
       </pre>
     </Fragment>
   )
+}
+
+function random() {
+  return Math.floor(Math.random() * 100);
 }
 
 const QUERY = {
@@ -47,34 +51,34 @@ const QUERY_RESULT = {
   metrics: {
     'spc_631284005076992363': {
       count: {
-        average: 10,
+        average: random(),
         max: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
         min: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
       },
       entrances: {
-        average: 10,
+        average: random(),
         peak: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
-        total: 100,
+        total: random(),
       },
       exits: {
-        average: 10,
+        average: random(),
         peak: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
-        total: 100,
+        total: random(),
       },
       targetUtilization: {
-        average: 10,
+        average: random(),
         durations: {
           0: 'not sure what this is',
           40: 'not sure what this is',
@@ -82,45 +86,45 @@ const QUERY_RESULT = {
           100: 'not sure what this is',
         },
         max: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
         min: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
       },
     },
     'spc_631245341374546129': {
       count: {
-        average: 10,
+        average: random(),
         max: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
         min: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
       },
       entrances: {
-        average: 10,
+        average: random(),
         peak: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
-        total: 100,
+        total: random(),
       },
       exits: {
-        average: 10,
+        average: random(),
         peak: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
-        total: 100,
+        total: random(),
       },
       targetUtilization: {
-        average: 10,
+        average: random(),
         durations: {
           0: 'not sure what this is',
           40: 'not sure what this is',
@@ -128,45 +132,45 @@ const QUERY_RESULT = {
           100: 'not sure what this is',
         },
         max: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
         min: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
       },
     },
     'spc_631011907855188085': {
       count: {
-        average: 10,
+        average: random(),
         max: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
         min: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
       },
       entrances: {
-        average: 10,
+        average: random(),
         peak: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
-        total: 100,
+        total: random(),
       },
       exits: {
-        average: 10,
+        average: random(),
         peak: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
-        total: 100,
+        total: random(),
       },
       targetUtilization: {
-        average: 10,
+        average: random(),
         durations: {
           0: 'not sure what this is',
           40: 'not sure what this is',
@@ -174,11 +178,11 @@ const QUERY_RESULT = {
           100: 'not sure what this is',
         },
         max: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
         min: {
-          value: 10,
+          value: random(),
           timestamp: '2019-01-01T00:00:00Z',
         },
       },
@@ -192,13 +196,41 @@ const ANALYTICS_REPORT = {
   query: QUERY,
   queryResult: {status: ResourceStatus.COMPLETE, data: QUERY_RESULT},
   hiddenSpaceIds: [],
-  selectedMetric: AnalyticsFocusedMetric.ENTRANCES,
+  /* selectedMetric */
   lastRunTimestamp: '2019-01-01T00:00:00Z',
 };
 
+const ANALYTICS_REPORT_ENTRANCES = { ...ANALYTICS_REPORT, selectedMetric: AnalyticsFocusedMetric.ENTRANCES },
+      ANALYTICS_REPORT_MAX = { ...ANALYTICS_REPORT, selectedMetric: AnalyticsFocusedMetric.MAX },
+      ANALYTICS_REPORT_UTILIZATION = { ...ANALYTICS_REPORT, selectedMetric: AnalyticsFocusedMetric.UTILIZATION };
+
 storiesOf('Analytics Table', module)
-  .add('Default', () => (
-    <State initialState={ANALYTICS_REPORT}>
+  .add('With visits data', () => (
+    <State initialState={ANALYTICS_REPORT_ENTRANCES}>
+      {(state, setState) => (
+        <AnalyticsTable
+          spaces={SPACES}
+          analyticsReport={state}
+
+          onChangeHiddenSpaceIds={hiddenSpaceIds => setState({...state, hiddenSpaceIds})}
+        />
+    )}
+    </State>
+  ))
+  .add('With max count data', () => (
+    <State initialState={ANALYTICS_REPORT_MAX}>
+      {(state, setState) => (
+        <AnalyticsTable
+          spaces={SPACES}
+          analyticsReport={state}
+
+          onChangeHiddenSpaceIds={hiddenSpaceIds => setState({...state, hiddenSpaceIds})}
+        />
+    )}
+    </State>
+  ))
+  .add('With utiization data', () => (
+    <State initialState={ANALYTICS_REPORT_UTILIZATION}>
       {(state, setState) => (
         <AnalyticsTable
           spaces={SPACES}
