a232122876bd061517ddefa97494e95a29c066e5
Gus Cost <guscost@gmail.com>
2019-10-09 16:38:19 -0400

    code: hide intercom if impersonating (#474)

--- src/components/intercom/index.tsx
+++ src/components/intercom/index.tsx
@@ -3,10 +3,13 @@ import React, { Fragment } from 'react';
 import Intercom from 'react-intercom';
 import useRxStore from '../../helpers/use-rx-store';
 import UserStore from '../../rx-stores/user';
+import ImpersonateStore from '../../rx-stores/impersonate';
 
 export function IntercomDensity() {
 
   const user = useRxStore(UserStore);
+  const impersonate = useRxStore(ImpersonateStore);
+  const notImpersonating = !impersonate || !impersonate.enabled || !impersonate.selectedUser;
 
   let intercomUser = {};
   if (user.data && user.data.hasOwnProperty("id")) {
@@ -18,7 +21,7 @@ export function IntercomDensity() {
       organization_name: user.data.organization.name,
     }
   }
-  if (process.env.REACT_APP_INTERCOM_APP_ID) {
+  if (process.env.REACT_APP_INTERCOM_APP_ID && notImpersonating) {
     return (
       <Intercom appID={process.env.REACT_APP_INTERCOM_APP_ID} { ...intercomUser } />
     )
