0f7b2ef384d02bea09284c5b2b264c4c3a44ba50
Robert Grazioli <rjgrazio@gmail.com>
2020-02-25 08:30:22 -0500

    huh

--- src/components/spaces-live-events/index.tsx
+++ src/components/spaces-live-events/index.tsx
@@ -145,7 +145,7 @@ export default function SpacesLiveEvents({space, doorway}: {space: CoreSpace, do
                 </text>
               </g> : null}
             </g>
-          </Fragment>;StylesProvider
+          </Fragment>;
         })}
       </svg>
     </div>
