4e1adbccb86ee6fc89ac5417d34a350fda4bf3ad
Ryan Gaus <rsg1egoman@gmail.com>
2020-05-26 13:29:18 -0400

    chore: add login error page to show errors that happen while logging in

---
+++ src/components/login-error/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+export default function LoginError() {
+  const loginError = window.localStorage.auth0LoginError;
+  delete window.localStorage.auth0LoginError;
+  return (
+    <div className={styles.centered}>
+      <div className={styles.navbarShell} />
+
+      <h1>Login Error: {loginError}</h1>
+      <p><a href="#/login">Click here to go back to the login page.</a></p>
+
+      {/* eslint-disable jsx-a11y/iframe-has-title */}
+      {/* Log out of the global auth0 session, so that clicking on the "login" link goes back to the login page */}
+      <iframe style={{display: 'none'}} src="https://densityio.auth0.com/v2/logout"></iframe>
+      {/* eslint-enable jsx-a11y/iframe-has-title */}
+    </div>
+  );
+}
---
+++ src/rx-actions/route-transition/login-error.ts
@@ -0,0 +1,5 @@
+export const ROUTE_TRANSITION_LOGIN_ERROR = 'ROUTE_TRANSITION_LOGIN_ERROR';
+
+export default async function routeTransitionLoginError(dispatch) {
+  dispatch({ type: ROUTE_TRANSITION_LOGIN_ERROR });
+}
