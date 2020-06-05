1432810099a5e2e6dc0dd0893d1484ca2cee7c75
John Shanley <john.michael.shanley@gmail.com>
2020-06-04 20:23:10 -0400

    Use constants for legacy redux action types

--- src/types/redux.ts
+++ src/types/redux.ts
@@ -1,21 +1,22 @@
 import { CoreSpace } from '@density/lib-api-types/core-v2/spaces';
-// import { COLLECTION_SPACES_SET } from '../actions/collection/spaces/set';
-// import { COLLECTION_SPACES_ERROR } from '../actions/collection/spaces/error';
+import { COLLECTION_SPACES_SET } from '../rx-actions/collection/spaces-legacy/set';
+import { COLLECTION_SPACES_ERROR } from '../rx-actions/collection/spaces-legacy/error';
+import { COLLECTION_SPACE_HIERARCHY_SET } from '../rx-actions/collection/space-hierarchy/set';
+import { TRANSITION_TO_SHOW_MODAL, SHOW_MODAL } from '../rx-actions/modal/show';
+import { TRANSITION_TO_HIDE_MODAL, HIDE_MODAL } from '../rx-actions/modal/hide';
+import { UPDATE_MODAL } from '../rx-actions/modal/update';
 
-// FIXME: The below 'COLLECTION_SPACES_SET' and 'COLLECTION_SPACES_ERROR' should use the constants
-// imported above, but if I do that, it types it as `string` and not the constant string it is set
-// to. Please point this point in a review!
-export type CollectionSpacesSet = { type: 'COLLECTION_SPACES_SET', data: Array<CoreSpace> };
-export type CollectionSpacesError = { type: 'COLLECTION_SPACES_ERROR', error: Error | string };
-export type CollectionSpaceHierarchySet = { type: 'COLLECTION_SPACE_HIERARCHY_SET', data: Array<CoreSpace> };
+export type CollectionSpacesSet = { type: typeof COLLECTION_SPACES_SET, data: Array<CoreSpace> };
+export type CollectionSpacesError = { type: typeof COLLECTION_SPACES_ERROR, error: Error | string };
+export type CollectionSpaceHierarchySet = { type: typeof COLLECTION_SPACE_HIERARCHY_SET, data: Array<CoreSpace> };
 
-export type TransitionToShowModal = { type: 'TRANSITION_TO_SHOW_MODAL', name: string, data: object };
-export type ShowModal = { type: 'SHOW_MODAL' };
+export type TransitionToShowModal = { type: typeof TRANSITION_TO_SHOW_MODAL, name: string, data: object };
+export type ShowModal = { type: typeof SHOW_MODAL };
 
-export type TransitionToHideModal = { type: 'TRANSITION_TO_HIDE_MODAL', name: string, data: object };
-export type HideModal = { type: 'HIDE_MODAL' };
+export type TransitionToHideModal = { type: typeof TRANSITION_TO_HIDE_MODAL, name: string, data: object };
+export type HideModal = { type: typeof HIDE_MODAL };
 
-export type UpdateModal = { type: 'UPDATE_MODAL' };
+export type UpdateModal = { type: typeof UPDATE_MODAL };
 
 export type ReduxAction = (
   CollectionSpacesSet |
